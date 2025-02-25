da7bb468eb173efae9ab669fd9c19d4d723dfe5b
sganeshwani <ganeshwani2003@gmail.com>
2025-02-26 02:47:21 +0530

    [Sahil Ganeshwani] Add UC2 - EmployeePayrollController with HTTP methods

--- src/main/java/com/bridgelabz/employeepayroll/controller/EmployeePayrollController.java
+++ src/main/java/com/bridgelabz/employeepayroll/controller/EmployeePayrollController.java
@@ -1,16 +1,32 @@
 package com.bridgelabz.employeepayroll.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/employeepayroll")
 public class EmployeePayrollController {
 
+    // GET request - Retrieve a welcome message
     @GetMapping("/welcome")
-    public String welcomeMessage() {
+    public String getWelcomeMessage() {
         return "Welcome to Employee Payroll App!";
     }
-}
 
+    // POST request - Add a new employee (Dummy message for now)
+    @PostMapping("/add")
+    public String addEmployee() {
+        return "New Employee Added Successfully!";
+    }
+
+    // PUT request - Update employee details (Dummy message for now)
+    @PutMapping("/update")
+    public String updateEmployee() {
+        return "Employee Details Updated!";
+    }
+
+    // DELETE request - Delete employee (Dummy message for now)
+    @DeleteMapping("/delete")
+    public String deleteEmployee() {
+        return "Employee Deleted!";
+    }
+}
