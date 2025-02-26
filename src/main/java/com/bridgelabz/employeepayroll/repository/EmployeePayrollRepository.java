8fc698de55fbde555320a86727d422eee4862684
Sahil Ganeshwani <156346043+sganeshwani@users.noreply.github.com>
2025-02-27 01:55:00 +0530

    Delete src/main/java/com/bridgelabz/employeepayroll/repository directory

--- src/main/java/com/bridgelabz/employeepayroll/repository/EmployeePayrollRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bridgelabz.employeepayroll.repository;
-
-import com.bridgelabz.employeepayroll.model.EmployeePayroll;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeePayrollRepository extends JpaRepository<EmployeePayroll, Long> {
-}
