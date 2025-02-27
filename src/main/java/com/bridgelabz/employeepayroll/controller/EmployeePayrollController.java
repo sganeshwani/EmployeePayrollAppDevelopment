6446b846eb82c56637222980e452ea1bc7e5d523
sganeshwani <ganeshwani2003@gmail.com>
2025-02-27 15:14:15 +0530

    [Sahil Ganeshwani] Add lombok library for logging

--- src/main/java/com/bridgelabz/employeepayroll/controller/EmployeePayrollController.java
+++ src/main/java/com/bridgelabz/employeepayroll/controller/EmployeePayrollController.java
@@ -3,6 +3,7 @@ package com.bridgelabz.employeepayroll.controller;
 import com.bridgelabz.employeepayroll.dto.EmployeeDTO;
 import com.bridgelabz.employeepayroll.model.Employee;
 import com.bridgelabz.employeepayroll.service.EmployeeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/employeepayroll")
+@Slf4j  // Lombok Logging Annotation
 public class EmployeePayrollController {
 
     @Autowired
@@ -18,30 +20,35 @@ public class EmployeePayrollController {
     // Get all employees
     @GetMapping("/get")
     public List<Employee> getAllEmployees() {
+        log.info("Fetching all employees");
         return employeeService.getAllEmployees();
     }
 
     // Get employee by ID
     @GetMapping("/get/{id}")
     public Employee getEmployeeById(@PathVariable int id) {
+        log.info("Fetching employee with ID: {}", id);
         return employeeService.getEmployeeById(id);
     }
 
     // Add new employee
     @PostMapping("/add")
     public Employee addEmployee(@RequestBody EmployeeDTO employeeDTO) {
+        log.info("Adding new employee: {}", employeeDTO);
         return employeeService.addEmployee(employeeDTO);
     }
 
     // Update employee details
     @PutMapping("/update/{id}")
     public Employee updateEmployee(@PathVariable int id, @RequestBody EmployeeDTO employeeDTO) {
+        log.info("Updating employee with ID: {}", id);
         return employeeService.updateEmployee(id, employeeDTO);
     }
 
     // Delete employee
     @DeleteMapping("/delete/{id}")
     public String deleteEmployee(@PathVariable int id) {
+        log.info("Deleting employee with ID: {}", id);
         employeeService.deleteEmployee(id);
         return "Employee with ID " + id + " deleted successfully!";
     }
